901e42a26d6b5257503d1e6e7f0c3784c36d0ed4
Aldo <aohilario.0528@gmail.com>
2020-08-21 22:05:42 -0500

    Penultimo commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// SCROLL ANIMATION
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash);
+      if (destino.length == 0) {
+        destino = $('a[name="' + this.hash.substr(1) + '"]');
+      }
+      if (destino.length == 0) {
+        destino = $('html');
+      }
+      $('html, body').animate({ scrollTop: destino.offset().top }, 800);
+      return false;
+    });
+  });
